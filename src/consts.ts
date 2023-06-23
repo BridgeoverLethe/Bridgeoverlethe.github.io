0a0b0cbcaef66ecdf5db5b7185360361cc148bbe
Nnikhil Bm <nofunda@hotmail.com>
2023-06-24 00:42:43 +0530

    Sort of working

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Nik Makes a Website.';
-export const SITE_DESCRIPTION = 'Hi! I like coffe, figuring out how to make the net better, and figuring stuff out. Thats what this site is about.';
+export const SITE_TITLE = 'Nik Makes a Website';
+export const SITE_DESCRIPTION = 'Experiemnts in LLMs, code, poorly considered ambitions and persistence bordering on obsession';
