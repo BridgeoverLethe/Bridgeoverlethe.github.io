422caca530c68b6e4fb97ffbf57ffeae3188f583
Nnikhil Bm <nofunda@hotmail.com>
2023-07-06 19:48:36 +0530

    Nav and desc

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Nik Makes a Website';
-export const SITE_DESCRIPTION = 'Experiemnts in LLMs, code, poorly considered ambitions and persistence bordering on obsession';
+export const SITE_TITLE = 'Cogito ergo caffeinatus';
+export const SITE_DESCRIPTION = 'Absolutely assume that everything on this website is a work in progress. This exists because I love lerarning how to actually use a thing, in this case LLMs, and because building things is fun! My main focus is trust and safety, and figuring things out. Reach out!  ';
